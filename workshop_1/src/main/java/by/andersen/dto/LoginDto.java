d75f655380b281ff6bfd7ac0517aa57d34b639b7
Smaktulka <evgeniybeketov304@gmail.com>
2024-12-22 16:44:55 +0300

    feat(workshop_1): add authentication
    This commit adds logic for login (implemented in AuthService). AuthService uses LoginDto and PasswordHashUtils for login method.
    PasswordHashUtils implements methods for creating password hash and verifying password by stored hash.

---
+++ workshop_1/src/main/java/by/andersen/dto/LoginDto.java
@@ -0,0 +1,13 @@
+package by.andersen.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+  private String userName;
+  private char[] password;
+}
---
+++ workshop_1/src/main/java/by/andersen/service/AuthService.java
@@ -0,0 +1,32 @@
+package by.andersen.service;
+
+import by.andersen.dto.LoginDto;
+import by.andersen.entity.User;
+import by.andersen.repository.UserRepository;
+import by.andersen.utils.PasswordHashUtils;
+import by.andersen.utils.Result;
+import java.util.Optional;
+
+public class AuthService {
+  private final UserRepository userRepository;
+
+  public AuthService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public Result<User> login(LoginDto loginDto) {
+    Optional<User> optionalUser = userRepository.getByName(loginDto.getUserName());
+    if (!optionalUser.isPresent()) {
+      return Result.error("User with name " + loginDto.getUserName() + " is not found");
+    }
+
+    boolean isPasswordValid = PasswordHashUtils
+        .verifyPassword(loginDto.getPassword(), optionalUser.get().getPasswordHash());
+
+    if (!isPasswordValid) {
+      return Result.error("Invalid password");
+    }
+
+    return Result.ok(optionalUser.get());
+  }
+}
