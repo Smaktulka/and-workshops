8a72ff1c88eca325eef96d07c181fd2a82bd0ca9
Smaktulka <evgeniybeketov304@gmail.com>
2024-12-22 16:51:35 +0300

    feat(workshop_1): add CommandPrinter class
    CommandPrinter is used for printing values from Command enums

---
+++ workshop_1/src/main/java/by/andersen/utils/CommandPrinter.java
@@ -0,0 +1,14 @@
+package by.andersen.utils;
+
+public class CommandPrinter {
+  public static <E extends Enum<E>> void printEnumValues(Class<E> enumClass) {
+    if (enumClass == null) {
+      throw new RuntimeException("Enum class cannot be null");
+    }
+
+    System.out.println("Available commands:");
+    for (E enumValue: enumClass.getEnumConstants()) {
+      System.out.println(enumValue);
+    }
+  }
+}
