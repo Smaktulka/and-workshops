92e4c25bcf27a1cdaa40eb4bd724efa0bc37ed13
Smaktulka <evgeniybeketov304@gmail.com>
2024-12-23 11:31:45 +0300

    feat(workshop_1): add methods for storing and loading repositories to/from file

--- workshop_1/src/main/java/by/andersen/context/RepositoryContext.java
+++ workshop_1/src/main/java/by/andersen/context/RepositoryContext.java
@@ -1,17 +1,44 @@
 package by.andersen.context;
 
 import by.andersen.repository.Repository;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RepositoryContext {
-  private final Map<Class<?>, Repository<?, ?>> repositoryMap = new HashMap<>();
+  private final Map<Class<?>, Repository<? extends Serializable, ?>> repositoryMap = new HashMap<>();
 
-  public <T, ID> void putRepository(Repository<T, ID> repository) {
+  public <T extends Serializable, ID> void putRepository(Repository<T, ID> repository) {
     repositoryMap.put(repository.getClass(), repository);
   }
 
-  public <T, ID> Repository<T, ID> getRepository(Class<? extends Repository<T, ID>> repositoryClass) {
+  public <T extends Serializable, ID> Repository<T, ID> getRepository(Class<? extends Repository<T, ID>> repositoryClass) {
     return (Repository<T, ID>) repositoryMap.get(repositoryClass);
+  }
+
+  public void saveToFile(String filePath) {
+    try (ObjectOutputStream oos =
+        new ObjectOutputStream(Files.newOutputStream(Paths.get(filePath)))) {
+      oos.writeObject(repositoryMap);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public void loadFromFile(String filePath) {
+    try (ObjectInputStream ois =
+        new ObjectInputStream(Files.newInputStream(Paths.get(filePath)))) {
+      Map<Class<?>, Repository<?, ?>> loadedRepositories = (Map<Class<?>, Repository<?, ?>>) ois.readObject();
+      repositoryMap.clear();
+      repositoryMap.putAll(loadedRepositories);
+    } catch (IOException | ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
