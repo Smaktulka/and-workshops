b63d6e744fb826e32a134e8c303cc69ebef52831
Smaktulka <evgeniybeketov304@gmail.com>
2024-12-23 11:34:15 +0300

    feat(workshop_1): update AppContext for storing and loading repositories to/from file

--- workshop_1/src/main/java/by/andersen/context/AppContext.java
+++ workshop_1/src/main/java/by/andersen/context/AppContext.java
@@ -3,6 +3,11 @@ package by.andersen.context;
 import by.andersen.repository.ReservationRepository;
 import by.andersen.repository.UserRepository;
 import by.andersen.repository.WorkspaceRepository;
+import by.andersen.utils.PropertiesUtils;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AppContext {
   private RepositoryContext repositoryContext;
+  private PropertiesUtils propertiesUtils;
 
   public static AppContext init() {
     RepositoryContext repositoryContext = new RepositoryContext();
@@ -24,6 +30,30 @@ public class AppContext {
     repositoryContext.putRepository(reservationRepository);
     repositoryContext.putRepository(workspaceRepository);
 
-    return new AppContext(repositoryContext);
+    PropertiesUtils propertiesUtils = PropertiesUtils.loadAppPropertiesFile();
+
+    Optional<File> optionalRepositoryStateFile = propertiesUtils.getRepositoryStateFile();
+
+    optionalRepositoryStateFile.ifPresent(
+        file -> loadRepositoriesFromFile(repositoryContext, file));
+
+    return new AppContext(repositoryContext, propertiesUtils);
+  }
+
+  public void onEnd() {
+    Optional<File> optionalRepositoryStateFile = propertiesUtils.getRepositoryStateFile();
+    optionalRepositoryStateFile.ifPresent(file -> repositoryContext.saveToFile(file.getPath()));
+  }
+
+  private static void loadRepositoriesFromFile(RepositoryContext repositoryContext, File repositoryStateFile) {
+    if (repositoryStateFile.exists()) {
+      repositoryContext.loadFromFile(repositoryStateFile.getPath());
+    } else {
+      try {
+        Files.createFile(repositoryStateFile.toPath());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
