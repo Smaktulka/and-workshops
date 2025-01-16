ad4a3558e7630a6787bcf2db0850eb8b494dbede
Smaktulka <evgeniybeketov304@gmail.com>
2025-01-16 15:32:30 +0300

    refactor(workshop_7): refactor DtoScanner
    This commit changes approach of scanning dtos. Now, reflection uses AllArgsConstructor to build an object with ability to validate it. Changes exception handling to more informative way

--- workshop_1/src/main/java/by/andersen/utils/DtoScanner.java
+++ workshop_1/src/main/java/by/andersen/utils/DtoScanner.java
@@ -1,82 +1,115 @@
 package by.andersen.utils;
 
+import by.andersen.dto.PeriodDto;
 import by.andersen.exception.DtoScanException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
 
 public class DtoScanner {
-  private static final String DATE_PATTERN = "yyyy-MM-dd";
-
   public static <T> Optional<T> scan(Class<T> dtoClass) {
+    if (dtoClass == null) {
+      throw new IllegalArgumentException("Dto class cannot be null");
+    }
+
     T instance;
     Scanner inputReader = new Scanner(System.in);
     try {
-      instance = dtoClass.getDeclaredConstructor().newInstance();
+      Field[] nonStaticFields = Arrays.stream(dtoClass.getDeclaredFields())
+          .filter(f -> !Modifier.isStatic(f.getModifiers()))
+          .toArray(Field[]::new);
 
-      Field[] fields = dtoClass.getDeclaredFields();
-      for (Field field: fields) {
-        field.setAccessible(true);
-        List<String> validValuesOrFormat = null;
-        if (field.getType().isEnum()) {
-          validValuesOrFormat = convertEnumValuesToList((Class<? extends Enum>) field.getType());
-        } else if (field.getType() == LocalDate.class) {
-          validValuesOrFormat = new ArrayList<>();
-          validValuesOrFormat.add(DATE_PATTERN);
-        }
+      List<Class> types = Arrays.stream(nonStaticFields).map(Field::getType)
+          .collect(Collectors.toCollection(ArrayList::new));
 
-        if (validValuesOrFormat != null) {
-          System.out.printf("%s(%s): ", field.getName(), validValuesOrFormat);
-        } else {
-          System.out.printf("%s: ", field.getName());
-        }
+      List<Object> parsedInputs = new ArrayList<>();
+      for (Field field: nonStaticFields) {
+        List<String> possibleValuesOrFormat = getPossibleValuesOrFormat(field);
+        printPossibleValuesOrFormatForField(possibleValuesOrFormat, field.getName());
 
         String input = inputReader.nextLine();
-
-        setInputToField(instance, field, input);
+        Object parsedInput = parseInput(field.getType(), input);
+        parsedInputs.add(parsedInput);
       }
-    } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
-             NoSuchMethodException e) {
-      throw new DtoScanException(e);
-    } catch (IllegalArgumentException | DateTimeException e) {
-      System.out.println("Invalid input!");
+
+      instance = dtoClass
+          .getDeclaredConstructor(types.toArray(Class[]::new))
+          .newInstance(parsedInputs.toArray(Object[]::new));
+
+    } catch  (IllegalArgumentException | DateTimeException e) {
+      outputInvalidInputMessage(e.getMessage());
       return Optional.empty();
+    } catch (NotImplementedException | InvocationTargetException | InstantiationException |
+             NoSuchMethodException | IllegalAccessException e) {
+      Throwable target = e.getCause();
+      if (target.getClass().equals(DateTimeException.class)) {
+        outputInvalidInputMessage(target.getMessage());
+        return Optional.empty();
+      }
+
+      throw new DtoScanException(e);
     }
 
     return Optional.of(instance);
   }
 
-  private static <T> void setInputToField(T instance, Field field, String input)
-      throws IllegalAccessException {
-    if (field.getType() == int.class || field.getType() == Integer.class) {
-      field.set(instance, Integer.parseInt(input));
-    } else if (field.getType() == double.class || field.getType() == Double.class) {
-      field.set(instance, Double.parseDouble(input));
-    } else if (field.getType() == long.class || field.getType() == Long.class) {
-      field.set(instance, Long.parseLong(input));
-    } else if (field.getType() == boolean.class || field.getType() == Boolean.class) {
-      field.set(instance, Boolean.parseBoolean(input));
-    } else if(field.getType() == BigDecimal.class) {
-      field.set(instance, BigDecimal.valueOf(Double.parseDouble(input)));
-    } else if (field.getType().isEnum()) {
-      Class<? extends Enum> enumClass = (Class<? extends Enum>) field.getType();
-      Enum<?> enumConstant = Enum.valueOf(enumClass, input.toUpperCase());
-      field.set(instance, enumConstant);
+  private static void outputInvalidInputMessage(String errorMessage) {
+    System.out.println(errorMessage);
+    System.out.println("Invalid input!");
+  }
+
+  private static List<String> getPossibleValuesOrFormat(Field field) {
+    if (field.getType().isEnum()) {
+      return convertEnumValuesToList((Class<? extends Enum>) field.getType());
     } else if (field.getType() == LocalDate.class) {
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
-      LocalDate date = LocalDate.parse(input, formatter);
-      field.set(instance, date);
+      return new ArrayList<>(Collections.singleton(PeriodDto.DATE_PATTERN));
+    }
+
+    return new ArrayList<>();
+  }
+
+  private static void printPossibleValuesOrFormatForField(List<String> possibleValuesOrFormat, String fieldName) {
+    if (!possibleValuesOrFormat.isEmpty()) {
+      System.out.printf("%s(%s): ", fieldName, possibleValuesOrFormat);
     } else {
-      field.set(instance, input);
+      System.out.printf("%s: ", fieldName);
+    }
+  }
+
+  private static Object parseInput(Class type, String input)
+      throws IllegalAccessException, NotImplementedException {
+    if (type == int.class || type == Integer.class) {
+      return Integer.parseInt(input);
+    } else if (type == double.class || type == Double.class) {
+      return Double.parseDouble(input);
+    } else if (type == long.class || type == Long.class) {
+      return Long.parseLong(input);
+    } else if (type == boolean.class || type == Boolean.class) {
+      return Boolean.parseBoolean(input);
+    } else if(type == BigDecimal.class) {
+      return BigDecimal.valueOf(Double.parseDouble(input));
+    } else if (type.isEnum()) {
+      Class<? extends Enum> enumClass = (Class<? extends Enum>) type;
+      return Enum.valueOf(enumClass, input.toUpperCase());
+    } else if (type == LocalDate.class) {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PeriodDto.DATE_PATTERN);
+      return LocalDate.parse(input, formatter);
+    } else if (type == String.class){
+      return input;
+    } else {
+      throw new NotImplementedException("Scan not implemented for " + type);
     }
   }
 
