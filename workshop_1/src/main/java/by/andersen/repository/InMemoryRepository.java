c04ce7bd102c940c484109539aa20ba36859f5fc
Smaktulka <evgeniybeketov304@gmail.com>
2025-01-17 11:56:58 +0300

    refactor(workshop_8): add abstract method updateLastId for InMemoryRepository

--- workshop_1/src/main/java/by/andersen/repository/InMemoryRepository.java
+++ workshop_1/src/main/java/by/andersen/repository/InMemoryRepository.java
@@ -1,6 +1,5 @@
 package by.andersen.repository;
 
-import by.andersen.entity.Identifiable;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,17 +7,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-public class InMemoryRepository<T extends Serializable, ID> implements Repository<T, ID>, Serializable {
+public abstract class InMemoryRepository<T extends Serializable, ID> implements Repository<T, ID>, Serializable {
   private final Map<ID, T> idEntityMap = new HashMap<>();
+  @Setter
   private volatile ID lastId;
 
   @Override
   public void save(T entity) {
-    ID id = ((Identifiable<ID>) entity).getId();
-    this.setLastId(id);
-    idEntityMap.put(id, entity);
+    this.updateLastId();
+    idEntityMap.put(lastId, entity);
   }
 
   @Override
@@ -36,7 +36,5 @@ public class InMemoryRepository<T extends Serializable, ID> implements Repositor
     idEntityMap.remove(id);
   }
 
-  private void setLastId(ID id) {
-    this.lastId = id;
-  }
+  public abstract void updateLastId();
 }
