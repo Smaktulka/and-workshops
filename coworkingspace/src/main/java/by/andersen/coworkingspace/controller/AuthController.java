9317559e90df5f356f1f4e7951180dc3f21f16e3
Smaktulka <evgeniybeketov304@gmail.com>
2025-02-06 10:24:51 +0300

    feat(workshop_13): add authorization to AuthController

--- coworkingspace/src/main/java/by/andersen/coworkingspace/controller/AuthController.java
+++ coworkingspace/src/main/java/by/andersen/coworkingspace/controller/AuthController.java
@@ -1,11 +1,16 @@
 package by.andersen.coworkingspace.controller;
 
 import by.andersen.coworkingspace.dto.LoginDto;
-import by.andersen.coworkingspace.entity.User;
+import by.andersen.coworkingspace.dto.RegisterDto;
+import by.andersen.coworkingspace.dto.TokensDto;
 import by.andersen.coworkingspace.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +25,24 @@ public class AuthController {
     this.authService = authService;
   }
 
-  @GetMapping("/login")
-  public ResponseEntity<User> login(@RequestBody LoginDto loginDto) {
-    User user = authService.login(loginDto);
-    return ResponseEntity.ok(user);
+  @PostMapping("/register")
+  public ResponseEntity<TokensDto> register(@RequestBody RegisterDto registerDto) {
+    TokensDto tokensDto = authService.register(registerDto);
+    return ResponseEntity.ok(tokensDto);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<TokensDto> login(@RequestBody LoginDto loginDto) {
+    TokensDto tokensDto = authService.login(loginDto);
+    return ResponseEntity.ok(tokensDto);
+  }
+
+  @PostMapping("/refresh-token")
+  public void refreshToken(
+      HttpServletRequest request,
+      HttpServletResponse response
+  ) throws IOException {
+    TokensDto tokensDto = authService.refreshToken(request);
+    new ObjectMapper().writeValue(response.getOutputStream(), tokensDto);
   }
 }
