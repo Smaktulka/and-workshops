8ba53d5dbb689d92bfa96c90c56a91379d689b33
Smaktulka <evgeniybeketov304@gmail.com>
2025-02-06 10:39:31 +0300

    feat(workshop_13): update User entity and UserRole to implement UserDetails

--- coworkingspace/src/main/java/by/andersen/coworkingspace/enums/UserRole.java
+++ coworkingspace/src/main/java/by/andersen/coworkingspace/enums/UserRole.java
@@ -1,6 +1,16 @@
 package by.andersen.coworkingspace.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum UserRole {
   CUSTOMER,
-  ADMIN
+  ADMIN;
+
+  public List<SimpleGrantedAuthority> getAuthorities() {
+    List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+    authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    return authorities;
+  }
 }
