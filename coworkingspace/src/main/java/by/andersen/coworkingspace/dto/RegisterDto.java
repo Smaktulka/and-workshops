4ea43c8feaeb4cc22a66caee7a821cf7f277e74c
Smaktulka <evgeniybeketov304@gmail.com>
2025-02-14 16:19:26 +0300

    refactor(workshop_15): update dtos

--- coworkingspace/src/main/java/by/andersen/coworkingspace/dto/RegisterDto.java
+++ coworkingspace/src/main/java/by/andersen/coworkingspace/dto/RegisterDto.java
@@ -1,9 +1,15 @@
 package by.andersen.coworkingspace.dto;
 
 import by.andersen.coworkingspace.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterDto {
   private String userName;
   private UserRole role;
